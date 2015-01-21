7b26d6507facc810e28224a1cdac8394346ee8ac
Wim Deprez <wim@tamale.nl>
2015-01-21 15:10:04 +0100

    customer request from 20141230:
    when the user clicks on "stock", show the current stock entry
    with the message "on <DATE> you confirmed X bags of cork, but we have not collected these bags yet. Do you want to adjust these figures?"
    + overwrite in the database.
    
    still TODO:
    * interchange weight and volume, calculate automagically as in the routetool (for both cork and parafine)
     --> if bags = 0 but kg > 0 => bags = ...
     --> if kg = 0 but bags > 0 => kg = ...

--- page-stock.php
+++ page-stock.php
@@ -9,22 +9,27 @@ get_header(); the_post(); ?>
 	<div class="content">
 	<?php
 	    // read http://codex.wordpress.org/WordPress_Nonces and stackoverflow.com/questions/1924939/php-request-vs-get-and-post
+        $aanmelding_id = -1;
   	    $zakken_kurk = 0;
 	    $kg_kurk = 0;
 	    $zakken_kaarsresten = 0;
 	    $kg_kaarsresten = 0;
+        $opmerkingen_form = "";
+        $handtekening = " -- ingegeven via MyRecy";
 
-    	if(wp_verify_nonce( $_POST["_wpnonce"], 'nieuwe_aanmelding_'.get_current_user_id() ))
+  	    require_once("secure/db.php");
+
+        // 1. bestaat er een $_POST, dan heeft de gebruiker waarschijnlijk een nieuwe aanmelding gedaan. Security check:
+    	if(wp_verify_nonce( $_POST["_wpnonce"], 'nieuwe_aanmelding_'.get_current_user_id().$_POST["aanmelding_id"] ))
     	{
-    	    require_once("secure/db.php");
             $user_ID = get_current_user_id();
-
+            
     	    $zakken_kurk = $MYRECY_mysqli->real_escape_string($_POST["zakken_kurk"]);
     	    $kg_kurk = $MYRECY_mysqli->real_escape_string($_POST["kg_kurk"]);
     	    $zakken_kaarsresten = $MYRECY_mysqli->real_escape_string($_POST["zakken_kaarsresten"]);
     	    $kg_kaarsresten = $MYRECY_mysqli->real_escape_string($_POST["kg_kaarsresten"]);
-    	    $opmerkingen = $MYRECY_mysqli->real_escape_string($_POST["opmerkingen"]);
-    	    $opmerkingen_met_handtekening = $MYRECY_mysqli->real_escape_string($_POST["opmerkingen"]." -- ingegeven via MyRecy");
+    	    $opmerkingen_form = $MYRECY_mysqli->real_escape_string($_POST["opmerkingen"]);
+    	    $opmerkingen_met_handtekening = $MYRECY_mysqli->real_escape_string($_POST["opmerkingen"].$handtekening);
     	    
     	    if ($result = $MYRECY_mysqli->query("SELECT ophaalpunten.id, ophaalpunten.contactpersoon FROM wordpress_link, ophaalpunten WHERE wordpress_userid = $user_ID and ophaalpunt_id = ophaalpunten.id"))
     	    {
@@ -32,7 +37,7 @@ get_header(); the_post(); ?>
                 if($result->num_rows < 1)
                 {
                     // no results found, so why even bother? quit! + show error message for users to contact adminstration
-		    show_myrecy_message("error", "Geen ophaalpunt gevonden dat overeenkomt met uw gebruikersnaam, stock is niet doorgegeven aan De Vlaspit. Gelieve even te wachten en opnieuw te proberen. Indien het probleem zich blijft voordoen, contacteer ons voor hulp.");
+                    show_myrecy_message("error", "Geen ophaalpunt gevonden dat overeenkomt met uw gebruikersnaam, stock is niet doorgegeven aan De Vlaspit. Gelieve even te wachten en opnieuw te proberen. Indien het probleem zich blijft voordoen, contacteer ons voor hulp.");
                     $result->close();
                     exit;
                 }
@@ -47,8 +52,23 @@ get_header(); the_post(); ?>
         	if(($_POST["zakken_kurk"] > 0) || ($_POST["kg_kurk"] > 0) || ($_POST["zakken_kaarsresten"] > 0) || ($_POST["kg_kaarsresten"] > 0) )
         	{
         	    // on query preparation: http://www.sanwebe.com/2013/03/basic-php-mysqli-usage and http://www.mustbebuilt.co.uk/php/insert-update-and-delete-with-mysqli/
-        	    $query = "INSERT INTO aanmelding (id, timestamp, ophaalpunt, contactpersoon, datum, zakken_kurk, kg_kurk, zakken_kaarsresten, kg_kaarsresten, opmerkingen, ophaalronde_datum, volgorde)
-        	                          VALUES(NULL, CURRENT_TIMESTAMP, ?, ?, CURRENT_DATE, ?, ?, ?, ?, ?, NULL, NULL)";
+
+                if($_POST["aanmelding_id"]  < 0)
+                {
+                    // "ophaalpunt zonder aanmeldingen, want ".$_POST["aanmelding_id"] < 0
+                    $query = "INSERT INTO aanmelding (id, timestamp, ophaalpunt, contactpersoon, datum, zakken_kurk, kg_kurk, zakken_kaarsresten, kg_kaarsresten, opmerkingen, ophaalronde_datum, volgorde)
+                                          VALUES(NULL, CURRENT_TIMESTAMP, ?, ?, CURRENT_DATE, ?, ?, ?, ?, ?, NULL, NULL)";
+                }
+                else
+                {
+                    // bestaande aanmelding wijzigen want $_POST["aanmelding_id"] >= 0
+                    $query = "UPDATE aanmelding SET ophaalpunt = ?, contactpersoon = ?, zakken_kurk = ?, kg_kurk = ?,
+                                    zakken_kaarsresten = ?, kg_kaarsresten = ?, opmerkingen = ?, datum = CURRENT_DATE
+                                    WHERE id = ".$_POST['aanmelding_id'];
+                }
+
+
+
                     $statement = $MYRECY_mysqli->prepare($query);
 
                     //bind parameters for markers, where (s = string, i = integer, d = double,  b = blob)
@@ -66,6 +86,7 @@ get_header(); the_post(); ?>
 
                     }
                     $statement->close();
+                    $aanmelding_id = mysql_insert_id();
 
                     // to really insert: http://stackoverflow.com/questions/16835753/inserting-data-to-table-mysqli-insert
         	}
@@ -80,6 +101,38 @@ get_header(); the_post(); ?>
         	}
     	}
 
+        // 2. bestaat er al een eerdere "aanmelding"? vul deze waarden dan in in het formulier
+        if ($result = $MYRECY_mysqli->query("SELECT aanmelding.* FROM wordpress_link, aanmelding WHERE wordpress_userid = $user_ID and ophaalpunt_id = ophaalpunt and volgorde is null ORDER BY timestamp DESC"))
+        {
+            //printf("Select returned %d rows.\n", $result->num_rows);
+            if($result->num_rows < 1)
+            {
+                show_myrecy_message("info", "Nog geen aanmelding gevonden, je kan hieronder een nieuwe aanmelding ingeven.");
+                $result->close();
+            }
+            else
+            {
+                $vorige_aanmelding = $result->fetch_object();
+                
+                $aanmelding_id = $vorige_aanmelding->id;
+                $kg_kurk = $vorige_aanmelding->kg_kurk;
+                $zakken_kurk = $vorige_aanmelding->zakken_kurk;
+                $kg_kaarsresten = $vorige_aanmelding->kg_kaarsresten;
+                $zakken_kaarsresten= $vorige_aanmelding->zakken_kaarsresten;
+                $opmerkingen_form = str_replace($handtekening,"",$vorige_aanmelding->opmerkingen); // haal de $handtekening uit de opmerkingen
+                $datum = $vorige_aanmelding->datum;
+                $contactpersoon = $vorige_aanmelding->contactpersoon;
+                show_myrecy_message("info", "Op ".strftime("%e %B %Y",strtotime($datum))." heeft u ".$zakken_kurk." zakken kurk en ".$zakken_kaarsresten." zakken kaarsresten aangemeld, maar deze zijn nog niet opgehaald. Wilt u deze aantallen aanpassen?");
+                $result->close();
+            }
+        }
+        else
+        {
+            // could not query DB, so why even bother? quit! + show error message for users to contact adminstration
+            show_myrecy_message("error", "De MyRecy-databank is momenteel niet bereikbaar, gelieve even te wachten en opnieuw te proberen. Indien het probleem zich blijft voordoen, contacteer ons voor hulp.");
+            exit;
+        }
+
 	  /// $_POST   mysqliescape() zowel HTML als PHP als SQL
 	  /// insert t_aanmelding;
 	  /// 		<p class="message">    Profile updated.<br />
@@ -88,7 +141,8 @@ get_header(); the_post(); ?>
 	?>
 	<form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post">
 
-    <?php wp_nonce_field( 'nieuwe_aanmelding_'.get_current_user_id() ); ?>
+    <?php wp_nonce_field( 'nieuwe_aanmelding_'.get_current_user_id().$aanmelding_id ); ?>
+    <input type="hidden" id="aanmelding_id" name="aanmelding_id" value="<?php echo $aanmelding_id; ?>" />
 
 
     <h3>Momenteel heeft ons ophaalpunt het volgende in stock</h3>
@@ -106,7 +160,7 @@ get_header(); the_post(); ?>
 		</tr>
 		<tr>
 			<th><label for="opmerkingen">Opmerkingen</label></th>
-			<td colspan="2"><textarea name="opmerkingen" id="opmerkingen" rows="5" cols="30"><?php /* to strip the MyRecy remark to avoid dubbels, thank you! */  echo $_POST["opmerkingen"]; ?></textarea></td>
+			<td colspan="2"><textarea name="opmerkingen" id="opmerkingen" rows="5" cols="30"><?php /* to strip the MyRecy remark to avoid dubbels, thank you! */  echo $opmerkingen_form; ?></textarea></td>
 		</tr>
 	</table>
 
