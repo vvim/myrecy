7fa6546c2f67cb8440dd656df87f311db20c5172
Wim Deprez <wim@tamale.nl>
2015-01-30 17:26:53 +0100

    myrecy bilingual (first step)
    read
     * http://codex.wordpress.org/Plugin_API/Filter_Reference/the_title
     * https://pippinsplugins.com/introduction-gettext-filter-wordpress/

--- functions.php
+++ functions.php
@@ -11,8 +11,34 @@ function enqueue_parent_theme_style() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
 
+/*
 
+to translate with gettext: see https://pippinsplugins.com/introduction-gettext-filter-wordpress/
+function pippin_gettext_demo($translated_text, $text , $domain)
+{
+	if( $translated_text == 'Add to Cart')
+	{
+		$translated_text = 'Purchase';
+	}
 
+	return $translated_text;
+}
+
+add_filter('gettext','pippin_gettext_demo',20,3);
+
+*/
+
+function translate_title( $title, $id = null ) {
+    global $user_ID;
+
+    if ( ($title == 'Contact') && ($user_ID == 'FRENCH SPEAKING') ) {
+        return 'TRANSLATE ME';
+    }
+
+    return $title;
+}
+
+add_filter( 'the_title', 'translate_title', 10, 2 );
 
 /* redirect users to front page after login */
 //    code not working for WP4? outdated?
