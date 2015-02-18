8a51a7438fc1741dd160810957e53517cbd8228f
Wim Deprez <wim@tamale.nl>
2015-02-18 11:53:18 +0100

    pages for the actual admin module

---
+++ page-beheer-myrecy-gebruiker-aanmaken.php
@@ -0,0 +1,32 @@
+<?php
+require_once("myrecy-func.php");
+
+
+
+  require_once('login-and-language-check.php');
+
+   if (!MYRECY_Administrator() )
+   {
+       // non-admins have no business here, back to base!
+        wp_redirect( home_url() );
+        exit;
+   }
+   
+   $current_user = wp_get_current_user();
+
+get_header();
+
+the_post(); ?>
+
+<div <?php post_class('post') ?>>
+	<h1 class="entry-title noinfo"><?php the_title(); ?></h1>
+	
+	<div class="content">
+	<?php
+		require_once("secure/db.php");
+    ?>
+
+ (enkel nog niet-gelinkte ophaalpunten tonen?)
+
+	</div>
+<?php get_footer() ?>
---
+++ page-beheer.php
@@ -0,0 +1,39 @@
+<?php
+require_once("myrecy-func.php");
+
+
+
+  require_once('login-and-language-check.php');
+
+   if (!MYRECY_Administrator() )
+   {
+       // non-admins have no business here, back to base!
+        wp_redirect( home_url() );
+        exit;
+   }
+   
+   $current_user = wp_get_current_user();
+
+get_header();
+
+the_post(); ?>
+
+<div <?php post_class('post') ?>>
+	<h1 class="entry-title noinfo"><?php the_title(); ?></h1>
+	
+	<div class="content">
+	<?php
+		require_once("secure/db.php");
+    ?>
+
+    <div id="info">
+        <p>Deze module is enkel toegankelijk voor <strong><?php echo  $current_user->user_login; ?></strong>. Hier kan u:</p>
+        <ul>
+            <li>Een nieuwe gebruiker van MyRecy <a href="../beheer-myrecy-gebruiker-aanmaken/">aanmaken</a> (enkel nog niet-gelinkte ophaalpunten tonen?).</li>
+            <li>Een gebruiker van MyRecy <a href="../beheer-myrecy-gebruiker-verwijderen">verwijderen</a>.</li>
+            <li>Uzelf aan een ophaalpunt naar keuze <a href="../beheer-ophaalpunt-linken-uzelf">linken</a>.</li>
+        </ul>
+    </div>
+
+	</div>
+<?php get_footer() ?>
