43a329269de119e9017ee6c6aef06a7d3c21eeae
Wim Deprez <wim@tamale.nl>
2015-03-13 20:09:46 +0100

    Adminmodule: Before deleting a user, ask confirmation

--- page-beheer-myrecy-gebruiker-verwijderen.php
+++ page-beheer-myrecy-gebruiker-verwijderen.php
@@ -64,7 +64,7 @@ the_post(); ?>
 
     <p>Selecteer de gebruiker die u wenst te wissen:</p>
 
-    <form action="<?php echo $_SERVER['REQUEST_URI']; ?>"  method="POST">
+    <form action="<?php echo $_SERVER['REQUEST_URI']; ?>"  onsubmit="return confirm_remove_user();" method="POST">
     <?php /* if user_id , then niet schrijven*/ MYRECY_Administrator(); ?>
 
             <select  name="teverwijderengebruiker" id="teverwijderengebruiker">
